c2c9af00655fd5b4b7ebe8f0cc1843a38799cd3b
Kushagra9027 <pkushagra287@gmail.com>
2025-01-07 10:48:11 +0530

    Add files via upload

---
+++ que2.py
@@ -0,0 +1,20 @@
+# # Using time 
+import time
+# time.sleep(3)
+# print("TIME'S UP!")#after 3 seconds message will be displayed
+# 1.Making a timer
+i=int(input("enter the number:"))
+while 0<i:
+    print(i)
+    time.sleep(1)
+    i=i-1  
+print("Times up")  
+    
+    
+
+
+    
+
+
+    
+
