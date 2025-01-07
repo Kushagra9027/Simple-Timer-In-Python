45c098ff24d63ce6105bdf38679133e4fb28827f
Kushagra9027 <pkushagra287@gmail.com>
2025-01-07 10:53:18 +0530

    Update que2.py

--- que2.py
+++ que2.py
@@ -1,8 +1,5 @@
 # # Using time 
 import time
-# time.sleep(3)
-# print("TIME'S UP!")#after 3 seconds message will be displayed
-# 1.Making a timer
 i=int(input("enter the number:"))
 while 0<i:
     print(i)
